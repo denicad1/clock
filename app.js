743489341ed6758fc34d0006d562601893b322eb
Anthony <denicad@msn.com>
2021-03-14 20:18:00 -0400

    need to fix alarm checking

--- app.js
+++ app.js
@@ -10,32 +10,63 @@
 const clockFace=document.querySelector('.main__clock__face');
 const wakeHour=document.getElementById('wake__hour');
 const wakeMin=document.getElementById('wake__min');
+const AMPM=document.getElementById('wake__M');
+const setAlarm=document.getElementById('__time__submit');
+let curTime;
+const alarms= new Set;
 
-
+//display time on page
 const timer= ()=>{
 const time=new Date();
 const minute= String(time.getMinutes()).padStart(2,0);
-const hour=String(time.getHours()).padStart(2,0);
+let hour=time.getHours();
 const secs=String(time.getSeconds()).padStart(2,0);
-const curTime= `${hour}:${minute}:${secs}`;
+const AMPM= hour<12?'AM':'PM';
+ if (hour>12) {
+     hour=String(hour%12).padStart(2,0);
+ }
+curTime= `${hour}:${minute}:${secs} ${AMPM}`;
 const intl=Intl.DateTimeFormat(`en-US`).format(time); 
-clockFace.textContent=`${curTime} \n
-${intl} is the date`;
+clockFace.innerHTML=`${curTime} <br> ${intl} is the date`;
+//need to move this but it is here for testing
+// alarmCheck(alarms);
+console.log(curTime===`08:17:00 PM`);
 }
-
-const options=(time,div)=>{
+//add options to selects under clock
+const options=(time,div,start)=>{
     let opt;
-    for (let j = 1; j <= time; j++) {
+    for (let j = start; j <= time; j++) {
     opt=document.createElement('option');
     opt.text=String(j).padStart(2,0);
     div.options.add(opt,j);
 }
 }
+//set alarm and add it to set to get rid of dups
+const alarmSet=()=>{
+const alarmHour=wakeHour.value;
+const alarmMin=wakeMin.value;
+const alarmAMPM=AMPM.value;
+const alarm=`${alarmHour}:${alarmMin}:00 ${alarmAMPM}`;
 
+alarms.add(alarm);
 
+}
+//convert set of alarms to arr and check to see if it time to set off alarm
+const alarmCheck=(Set)=>{
+    const alarmArr=Array.from(alarms);
+    alarmArr.forEach(alarm=>{
+        if(alarm===curTime){
+            console.log('alarm bell');
+            return true;
+        }
+    })
+    
+}
+setAlarm.addEventListener('click',alarmSet);
+//initialize
 const init=()=>{
     const time=setInterval(timer,1000);
-    options(12,wakeHour);
-options(60,wakeMin);
+    options(12,wakeHour,1);
+options(59,wakeMin,0);
 }
 init();
